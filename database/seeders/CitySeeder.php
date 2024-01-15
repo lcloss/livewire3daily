e838b8b94dc07fd9ee79d259a8c68dafa52557b6
lcloss <lcloss@gmail.com>
2024-01-15 19:27:15 +0000

    City Seeder fix and reduce for tests

--- database/seeders/CitySeeder.php
+++ database/seeders/CitySeeder.php
@@ -23,22 +23,37 @@ class CitySeeder extends Seeder
         // Skip first line
         fgetcsv($file);
 
+        $countriesList = [];
+
         // Read data from CSV file
         while (($data = fgetcsv($file)) !== false) {
             $name = $data[1];
             $country_name = $data[4];
-            $country = Country::where('name', $country_name)->first();
-            $city = City::where('name', $name)->where('country_id', $country->id)->first();
 
-            // Insert data into cities table
-            if ( ! $city) {
-                $city = City::create([
-                    'country_id' => $countries[$data[4]], // $country->id,
-                    'name' => $name,
-                    'lat' => $data[2],
-                    'lng' => $data[3],
-                    'population' => empty( $data[9] ) ? null : $data[9],
-                ]);
+            if ( ! array_key_exists( $country_name, $countriesList ) ) {
+                $country = Country::where('name', $country_name)->first();
+                $countriesList[$country_name] = [
+                    'id' => $country->id,
+                    'counter' => 1,
+                ];
+            } else {
+                $countriesList[$country_name]['counter']++;
+            }
+
+            // This is to avoid inserting more than 3 cities per country, for testing purposes
+            if ( $countriesList[$country_name]['counter'] < 3 ) {
+                $city = City::where('name', $name)->where('country_id', $countriesList[$country_name]['id'])->first();
+
+                // Insert data into cities table
+                if ( ! $city) {
+                    $city = City::create([
+                        'country_id' => $countries[$data[4]], // $country->id,
+                        'name' => $name,
+                        'lat' => $data[2],
+                        'lng' => $data[3],
+                        'population' => empty( $data[9] ) ? null : $data[9],
+                    ]);
+                }
             }
         }
 
